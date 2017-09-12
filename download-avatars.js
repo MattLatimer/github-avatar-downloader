f73d86ca9b19752aa0e06f7fe0ea644f6cf4d77c
Matt Latimer <matt.latimer@gmail.com>
2017-09-11 17:25:43 -0700

    builds valid url from variables

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,16 @@
+require('dotenv').config();
 const request = require('request');
+const githubUser = process.env.GITHUB_USER;
+const githubToken = process.env.GITHUB_ACCESS_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  const requestURL = 'https://' + githubUser + ':' + githubToken + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
